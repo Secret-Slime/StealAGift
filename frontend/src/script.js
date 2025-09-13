a1077d539a564f3ba74da9f1abe586af4b048c80
caleblandry1 <caleb.landry1@dcmail.ca>
2025-09-13 11:31:43 -0400

    html forms for login, registration, and adding gifts

---
+++ frontend/src/script.js
@@ -0,0 +1,52 @@
+
+
+//html gift form
+let giftFormHTML = `
+    <h1>Gift Form</h1>
+    <form id="giftForm">
+        <label for="giftName">Gift Name:</label>
+        <input type="text" id="giftName" name="giftName" required>
+        <br><br>
+        <label for="description" id="giftDescription">Description:</label>
+        <textarea id="description" name="description" required></textarea>
+        <br><br>
+        <label for="price" id="giftPrice">Price:</label>
+        <input type="number" id="price" name="price" required>
+        <br><br>
+        <label for="link" id="giftLink">Link:</label>
+        <input type="url" id="link" name="link">
+        <br><br>
+        <button type="submit">Add gift</button>
+    </form>
+`;
+
+// html login form
+let loginFormHTML = `
+    <h1>Login</h1>
+    <form id="loginForm">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br><br>    
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+        <button type="submit">Login</button>
+    </form>
+`;
+
+// html registration form
+let registrationFormHTML = `
+    <h1>Register</h1>
+    <form id="registrationForm">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <br><br>
+        <button type="submit">Register</button>
+    </form>
+`;
